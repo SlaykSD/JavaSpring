6b143d97c5c8cbee630b0b26e90c821c23a7dcfa
SlaykSD <60920077+SlaykSD@users.noreply.github.com>
2022-10-03 21:14:06 +0300

    add files for test and old files

---
+++ src/main/java/com/edu/ulab/app/storage/Storage.java
@@ -0,0 +1,24 @@
+package com.edu.ulab.app.storage;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+//TODO:1) Какая структура лучше?
+
+public  interface Storage<K,V>{
+    //todo создать хранилище в котором будут содержаться данные
+    // сделать абстракции через которые можно будет производить операции с хранилищем
+    // продумать логику поиска и сохранения
+    // продумать возможные ошибки
+    // учесть, что при сохранеии юзера или книги, должен генерироваться идентификатор
+    // продумать что у узера может быть много книг и нужно создать эту связь
+    // так же учесть, что методы хранилища принимают друго тип данных - учесть это в абстракции
+    public V save(V entity);
+    public V findById(K primaryKey);
+    public void delete(K primaryKey);
+    public V update(V entity);
+    public Iterable<V> getAllEntity();
+}
---
+++ src/test/resources/sql/2_insert_person_data.sql
@@ -0,0 +1,4 @@
+insert into ulab_edu.person (Person_ID, FULL_NAME, TITLE, AGE)
+values (1001, 'default uer', 'reader', 55);
+
+
